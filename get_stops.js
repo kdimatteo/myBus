aedadc32a48a973f9a4ca2a93e942afc667f8a8f
Keith DiMatteo <kdimatteo@mobiquityinc.com>
2014-09-26 13:21:36 -0400

    add args to get_stops tool

--- get_stops.js
+++ get_stops.js
@@ -1,14 +1,27 @@
 
+/**
+ * get stops for a specifc bus (we do not care about inbound/outbound)
+ * @usage
+ * node get_stops.js 93
+ * yields stop id and display_name for all stops on the 93 bus
+ */
 var request = require('request'),
   JSONStream = require('JSONStream'),
-  es = require('event-stream'),
-  u = 'http://proximobus.appspot.com/agencies/mbta/routes/93/stops.json';
+  colors = require('colors'),
+  es = require('event-stream');
+
+var u = 'http://proximobus.appspot.com/agencies/mbta/routes/' + process.argv[2] + '/stops.json';
 
 j = request({url: u})
   .pipe(JSONStream.parse())
   .pipe(es.mapSync(function (data) {
     data.items.forEach(function(r){
-      console.log(r.id + ": " + r.display_name);
+      if(r.id !== undefined){
+        id = r.id.green
+      } else {
+        id = r.id
+      }
+      console.log(id + ": " + r.display_name);
     });
   })
 );
